790ed4ca5d76a904f9470cd9ff773b04208b53cb
Tony Haddad <tony.haddad426@gmail.com>
2021-12-04 10:58:26 -0500

    Adding user sign up and sign up verification functionality via Cognito js client

--- src/Components/Authentication/AuthForm.js
+++ src/Components/Authentication/AuthForm.js
@@ -1,12 +1,25 @@
 import React, { useState, useRef } from "react";
 import classes from "./AuthForm.module.css";
 import Spinner from "../Styling/Spinner";
+import SignUpConfirm from "./SignUpConfirm/SignUpConfirm";
+
+import "cross-fetch/polyfill";
+import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
 
 function AuthForm(props) {
+  // move to config file
+  const poolData = {
+    UserPoolId: "us-east-1_jBfRxKNkx",
+    ClientId: "7kc4rfc9ehbh5al4nakqa3doja",
+  };
+
+  const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+  const [showVerifyCode, setShowVerifyCode] = useState(false);
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const usernameInputRef = useRef();
-  // const emailInputRef = useRef();
+  const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
   const switchAuthModeHandler = () => {
@@ -18,14 +31,14 @@ function AuthForm(props) {
     event.preventDefault();
     const enteredUsername = usernameInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
+    const enteredEmail = emailInputRef.current.value;
 
     // optional: add validation
     if (isLogin) {
-      setIsLoading(true);
       // if isLogin is true, then authenticate existing user
       if (enteredUsername === "Student" && enteredPassword) {
         console.log(
-          "Updating logged in state and setting current user state in app,js"
+          "Updating logged in state and setting current user state in app.js"
         );
         props.setIsLoggedIn(true);
         props.setCurrentUser(enteredUsername);
@@ -52,49 +65,94 @@ function AuthForm(props) {
           .catch((err) => console.log(err));
       }
     } else {
-      setIsLoading(true);
+      const email = new AmazonCognitoIdentity.CognitoUserAttribute({
+        Name: "email",
+        Value: enteredEmail,
+      });
+
       // else if isLogin is false, create new user
+      userPool.signUp(
+        enteredUsername,
+        enteredPassword,
+        [email],
+        null,
+        function (err, result) {
+          console.log("HI");
+          console.log(JSON.stringify(result));
+
+          if (err) {
+            alert(err.message || JSON.stringify(err));
+            return;
+          } else {
+            const cognitoUser = result.user;
+            console.log("user name is " + cognitoUser.getUsername());
+            setShowVerifyCode(true);
+          }
+        }
+      );
     }
   };
 
   return (
     <div>
-      <section className={classes.auth}>
-        <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-        <form onSubmit={submitHandler}>
-          <div className={classes.control}>
-            <label htmlFor="username">Username</label>
-            <input
-              type="username"
-              id="username"
-              required
-              ref={usernameInputRef}
-            />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              required
-              ref={passwordInputRef}
-            />
-          </div>
-          <div className={classes.actions}>
-            {!isLoading && (
-              <button>{isLogin ? "Login" : "Create Account"}</button>
-            )}
-            {isLoading && <Spinner />}
-            <button
-              type="button"
-              className={classes.toggle}
-              onClick={switchAuthModeHandler}
-            >
-              {isLogin ? "Create new account" : "Login with existing account"}
-            </button>
-          </div>
-        </form>
-      </section>
+      {showVerifyCode && (
+        <SignUpConfirm
+          setShowVerifyCode={setShowVerifyCode}
+          userPool={userPool}
+          enteredUsername={usernameInputRef.current.value}
+          setIsLog={setIsLogin}
+        />
+      )}
+      {!showVerifyCode && (
+        <section className={classes.auth}>
+          <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+          <form onSubmit={submitHandler}>
+            <div className={classes.control}>
+              <label htmlFor="username">Username</label>
+              <input
+                type="username"
+                id="username"
+                maxLength="20"
+                required
+                ref={usernameInputRef}
+              />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                maxLength="100"
+                required
+                ref={emailInputRef}
+              />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                maxLength="20"
+                required
+                ref={passwordInputRef}
+              />
+            </div>
+            <div className={classes.actions}>
+              {!isLoading && (
+                <button>{isLogin ? "Login" : "Create Account"}</button>
+              )}
+              {isLoading && <Spinner />}
+              <button
+                type="button"
+                className={classes.toggle}
+                onClick={switchAuthModeHandler}
+              >
+                {isLogin ? "Create new account" : "Login with existing account"}
+              </button>
+            </div>
+          </form>
+        </section>
+      )}
     </div>
   );
 }
---
+++ src/Components/Authentication/SignUpConfirm/SignUpConfirm.js
@@ -0,0 +1,57 @@
+import React, { useRef } from "react";
+import "cross-fetch/polyfill";
+import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
+
+function SignUpConfirm(props) {
+  const codeInputRef = useRef();
+  const cognitoUser = new AmazonCognitoIdentity.CognitoUser({
+    Username: props.enteredUsername,
+    Pool: props.userPool,
+  });
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredCode = codeInputRef.current.value;
+
+    cognitoUser.confirmRegistration(enteredCode, true, function (err, results) {
+      console.log(enteredCode);
+      if (err) {
+        alert(err);
+      } else {
+        console.log(results);
+        props.setShowVerifyCode(false); // close verify code component and bring user back to login/sign up homepage
+        props.setIsLogin(true) // switch to log in form instead of sign up
+      }
+    });
+  };
+
+  const resendCode = (event) => {
+    event.preventDefault();
+    cognitoUser.resendConfirmationCode(function (err) {
+      if (err) {
+        alert(err);
+      }
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="code">A verification code has been sent to the provided email address</label>
+          <input
+            type="text"
+            id="code"
+            maxLength="20"
+            required
+            placeholder="Enter verification code..."
+            ref={codeInputRef}
+          />
+          <button>Verify</button>
+        </div>
+        <button onClick={resendCode}>Resend Code</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUpConfirm;
